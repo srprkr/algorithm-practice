251366301d0d69650e839848ee0b3d0e8d5f754d
Sean Parker <seanrichardparker@gmail.com>
2025-08-20 06:41:52 -0400

    word pattern python

--- hashmaps/word_pattern.rb
+++ hashmaps/word_pattern.rb
@@ -2,17 +2,22 @@
 # @param {String} s
 # @return {Boolean}
 def word_pattern(pattern, s)
-        pa = pattern.split("")
-        s_arr = s.split(" ")
-        uniq_s_arr = s_arr.uniq
-        return false if pa.size != s_arr.count
-        hash = {}
+    p1 = pattern.chars
+    p2 = s.split(" ")
+    return false if p1.size != p2.size
+    hash = {}
 
-        pa.uniq.each_with_index do |l, i|
-            hash[l] = uniq_s_arr[i]
+    p1.each_with_index do |c, idx|
+        ptr = "pat_#{c}"
+        st = "word_#{p2[idx]}"
+        if hash[ptr].nil? && hash[st].nil?
+            hash[ptr] = idx
+            hash[st] = idx
+        elsif(!hash[ptr].nil? && !hash[st].nil? && hash[ptr] == hash[st])
+            next
+        else
+            return false
         end
-        pa.each_with_index do |l, i|
-            return false if hash[l] != s_arr[i]
-        end
-        true
+    end
+    true
 end
