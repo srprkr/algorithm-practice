92244346d39091ac2b76933516850e38fdd5d582
Sean Parker <seanrichardparker@gmail.com>
2025-12-30 23:42:20 -0500

    find median from data stream

--- heaps/find_median_from_data_stream.rb
+++ heaps/find_median_from_data_stream.rb
@@ -1,16 +1,35 @@
+require 'algorithms'
+
 class MedianFinder
-    def initialize()
-        @arr = []
+    def initialize
+        @max_heap = Containers::MaxHeap.new
+        @min_heap = Containers::MinHeap.new
     end
-
+    
     def add_num(num)
-        ind = @arr.bsearch_index {|x| x > num } || @arr.length
-        @arr.insert(ind, num)
+        if @max_heap.empty? || num <= @max_heap.max
+            @max_heap.push(num)
+        else
+            @min_heap.push(num)
+        end
+        balance
+    end
+    
+    def find_median
+        if @max_heap.size == @min_heap.size
+            return (@max_heap.max + @min_heap.min) / 2.0
+        else
+            return @max_heap.max.to_f
+        end
     end
 
-    def find_median()
-        n = @arr.size
-        m = n / 2
-        n.odd? ? @arr[m] : (@arr[m - 1] + @arr[m] ) / 2.0
+    private
+
+    def balance
+        if @max_heap.size < @min_heap.size
+            @max_heap.push(@min_heap.pop)
+        elsif @max_heap.size > @min_heap.size + 1
+            @min_heap.push(@max_heap.pop)
+        end
     end
 end
